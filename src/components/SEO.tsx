bc3352262635d4863c346edfce53f637e641e24e
lppduy <lephamphuongduy@gmail.com>
2024-06-07 15:51:19 +0700

    feat: add thumbnail for SEO

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -4,9 +4,10 @@ interface SEOProps {
   title: string;
   description: string;
   url: string;
+  image?: string; // Thêm thuộc tính image (tùy chọn)
 }
 
-const SEO: React.FC<SEOProps> = ({ title, description, url }) => {
+const SEO: React.FC<SEOProps> = ({ title, description, url, image }) => {
   return (
     <Head>
       <title>{title}</title>
@@ -14,10 +15,12 @@ const SEO: React.FC<SEOProps> = ({ title, description, url }) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
+      {image && <meta property="og:image" content={image} />}
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
+      {image && <meta name="twitter:image" content={image} />}
       <meta name="twitter:site" content={url} />
     </Head>
   );
