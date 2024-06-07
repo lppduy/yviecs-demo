40bf782f678d7be5c4b5e58034e642ebe2a54e4e
lppduy <lephamphuongduy@gmail.com>
2024-06-07 15:41:54 +0700

    setup: adjust style

--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,12 @@
 // src/theme.ts
 import { createTheme } from '@mui/material/styles';
-import { grey, blue, black  } from '@mui/material/colors';
+import { grey, blue  } from '@mui/material/colors';
 
 const primaryColor = '#261517';
-// const backgroundColor = '#261517';
 
 const lightTheme = createTheme({
   palette: {
     mode: 'light',
-    // background: {
-    //   default: backgroundColor,
-    // },
     primary: {
       main: primaryColor,
     },
